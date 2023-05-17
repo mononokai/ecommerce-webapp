8b87ce9686e3dad3dc40a94d1ab43ebbdd0b7e72
David Leach <leachda12@icloud.com>
2023-05-17 05:23:46 -0400

    discover page populated

--- products/products.py
+++ products/products.py
@@ -1,26 +1,38 @@
-from flask import Blueprint, render_template, session
+from flask import Blueprint, render_template, redirect, url_for, session, flash, request
 from sqlalchemy import text
 from db.db import conn
+from db.queries import full_product_select as fps
+from flask_wtf import FlaskForm
+from wtforms import (
+    StringField,
+    PasswordField,
+    SubmitField,
+    BooleanField,
+    IntegerField,
+    SelectField,
+    TextAreaField,
+    DateField,
+    DecimalField
+)
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 
 products_bp = Blueprint("products_bp", __name__, static_folder="static", template_folder="templates")
 
 
-@products_bp.route('discover/')
+@products_bp.route('/discover/')
 def discover():
-    print(session) # TODO: Remove
-    return render_template('products/discover.html')
+    products = conn.execute(text(f"{ fps } where vendor_product.inventory > 0;"), {}).fetchall()
 
+    return render_template('products/discover.html', products=products)
 
-@products_bp.route('<product_id>/')
+@products_bp.route('/discover/search/')
+def search():
+    return render_template('products/search.html')
+
+@products_bp.route('/product_page/<product_id>/')
 def product(product_id):
     return render_template('products/product_page.html')
 
-
-@products_bp.route('<product_id>/review/')
+@products_bp.route('/product_page/<product_id>/review/')
 def reviews(product_id):
     return render_template('products/review.html')
-
-
-@products_bp.route('search/')
-def search():
-    return render_template('products/search.html')
